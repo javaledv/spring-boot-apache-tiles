62ad62cf86a52103faef070eea288125a39df68c
denis <denislesheniuk@gmail.com>
2018-01-14 21:46:24 +0300

    Refactoring.

--- src/main/java/ml/ledv/spring/boot/apache/tiles/controller/ControllerMVC.java
+++ src/main/java/ml/ledv/spring/boot/apache/tiles/controller/ControllerMVC.java
@@ -1,4 +1,4 @@
-package ml.ledv.springMvc.controller;
+package ml.ledv.spring.boot.apache.tiles.config.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
