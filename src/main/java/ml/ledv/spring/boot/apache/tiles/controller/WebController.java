add3dce7090175bc359601336e7d34cac902bfb2
dzianis.leshaniuk <dzianis.leshaniuk@instinctools.ru>
2018-01-15 17:57:14 +0300

    Added some functionality for books page.

--- src/main/java/ml/ledv/spring/boot/apache/tiles/controller/WebController.java
+++ src/main/java/ml/ledv/spring/boot/apache/tiles/controller/WebController.java
@@ -1,23 +1,27 @@
 package ml.ledv.spring.boot.apache.tiles.controller;
 
 import ml.ledv.spring.boot.apache.tiles.db.entity.impl.BookEntity;
+import ml.ledv.spring.boot.apache.tiles.db.entity.impl.UserEntity;
 import ml.ledv.spring.boot.apache.tiles.db.service.BookService;
+import ml.ledv.spring.boot.apache.tiles.db.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class WebController {
 
     private BookService bookService;
+    private UserService userService;
 
     @Autowired
-    public WebController(final BookService bookService) {
+    public WebController(final BookService bookService,final UserService userService) {
         this.bookService = bookService;
+        this.userService = userService;
     }
 
     @RequestMapping("/")
@@ -30,8 +34,18 @@ public class WebController {
 
         final List<BookEntity> books = bookService.getAll();
         final ModelAndView modelAndView = new ModelAndView();
+        final boolean isFree = false;
 
-        modelAndView.addObject("books", books);
+        final Map <BookEntity, Boolean> bok = new HashMap<>();
+        for (BookEntity bookEntity : books){
+            if(userService.getUserByBook(bookEntity).isPresent()){
+                bok.put(bookEntity, true);
+            }else {
+                bok.put(bookEntity, false);
+            }
+        }
+
+        modelAndView.addObject("books", bok);
 
         return modelAndView;
     }
@@ -40,10 +54,20 @@ public class WebController {
     public ModelAndView getFreeBooks(){
 
         final List<BookEntity> books = bookService.getAll();
+        final Map<BookEntity, Boolean> freeBooks = new HashMap<>();
+
+        for (BookEntity bookEntity: books){
+            Optional<UserEntity> userOptional = userService.getUserByBook(bookEntity);
+
+            if(!userOptional.isPresent()){
+                freeBooks.put(bookEntity, false);
+            }
+        }
+
         final ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.addObject("books", null);
-
+        final boolean isFree = true;
+        modelAndView.addObject("books", freeBooks);
+        modelAndView.addObject("isFree", isFree);
         return modelAndView;
     }
 }
--- src/main/webapp/WEB-INF/jsp/books.jsp
+++ src/main/webapp/WEB-INF/jsp/books.jsp
@@ -3,13 +3,21 @@
 <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Show
-        <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-        <li><a href="/books">All</a></li>
-        <li><a href="books/free">Free</a></li>
-    </ul>
+
+<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group mr-2" role="group" aria-label="First group">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Show
+                <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="/books">All</a></li>
+                <li><a href="/books/free">Free</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="btn-group mr-2" role="group" aria-label="Second group">
+        <button type="button" class="btn btn-secondary">Add book</button>
+    </div>
 </div>
 
 <c:if test="${not empty books}">
@@ -30,13 +38,23 @@
         <tbody>
         <c:forEach var="listValue" items="${books}">
             <tr>
-                <td>${listValue.id}</td>
-                <td>${listValue.name}</td>
-                <td>${listValue.description}</td>
-                <td>${listValue.isbn}</td>
-                <td>${listValue.author}</td>
-                <td>${listValue.publisher}</td>
-                <td>${listValue.content}</td>
+                <td>${listValue.key.id}</td>
+                <td>${listValue.key.name}</td>
+                <td>${listValue.key.description}</td>
+                <td>${listValue.key.isbn}</td>
+                <td>${listValue.key.author}</td>
+                <td>${listValue.key.publisher}</td>
+                <td>${listValue.key.content}</td>
+                <c:if test="${isFree}">
+                    <td>
+                        <button type="button" class="btn btn-outline-info">book it</button>
+                    </td>
+                </c:if>
+                <td>
+                    <c:if test="${listValue.value}">
+                        <button type="button" class="btn btn-outline-info">de-reserve</button>
+                    </c:if>
+                </td>
             </tr>
         </c:forEach>
         </tbody>
