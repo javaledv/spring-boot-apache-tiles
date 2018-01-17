d2c3920c3f894998ec24dd19ad1c391150259a1c
dzianis.leshaniuk <dzianis.leshaniuk@instinctools.ru>
2018-01-17 17:40:49 +0300

    Refactoring

--- src/main/java/ml/ledv/spring/boot/apache/tiles/config/RepositoryConfig.java
+++ src/main/java/ml/ledv/spring/boot/apache/tiles/config/RepositoryConfig.java
@@ -1,4 +1,4 @@
-package ml.ledv.spring.boot.apache.tiles.db.repository;
+package ml.ledv.spring.boot.apache.tiles.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration
 @EnableJpaRepositories(basePackages = "ml.ledv.spring.boot.apache.tiles.db.repository.jpa")
 @EnableMongoRepositories(basePackages = "ml.ledv.spring.boot.apache.tiles.db.repository.mongo")
-public class RepoConfig {
+public class RepositoryConfig {
 }
