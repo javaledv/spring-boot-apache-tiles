daf33ca4a28f15624adc9ea6f816fa0a20afe4bb
dzianis.leshaniuk <dzianis.leshaniuk@instinctools.ru>
2018-01-26 13:05:02 +0300

    Test commit for Jenkins 3.

--- src/main/java/ml/ledv/spring/boot/apache/tiles/Main.java
+++ src/main/java/ml/ledv/spring/boot/apache/tiles/Main.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
